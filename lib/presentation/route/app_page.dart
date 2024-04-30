f082183f9205f8dd00489da7327be0521d7f8bca
hanggmaii <hangmtn.00@gmail.com>
2024-04-30 17:41:16 +0700

    :update: update code

--- lib/presentation/route/app_page.dart
+++ lib/presentation/route/app_page.dart
@@ -1,5 +1,17 @@
+import 'package:get/get.dart';
+
+import '../screen/splash/splash_binding.dart';
+import '../screen/splash/splash_screen.dart';
+import 'app_route.dart';
+
 class AppPage {
   AppPage._();
 
-  static final pages = [];
+  static final pages = [
+    GetPage(
+      name: AppRoute.splashScreen,
+      page: () => const SplashScreen(),
+      binding: SplashBinding(),
+    )
+  ];
 }
