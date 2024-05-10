fb8b1aac9972ebd52a9b2514a5833d90e527d3aa
hanggmaii <hangmtn.00@gmail.com>
2024-05-10 19:00:05 +0700

    :create: app widget

---
+++ lib/presentation/widget/app_header.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import '../../utils/app_image.dart';
+import '../theme/app_color.dart';
+import '../theme/app_text_theme.dart';
+import 'app_image_widget.dart';
+import 'app_touchable.dart';
+
+class AppHeader extends StatelessWidget {
+  final String? title;
+  final String? hintContent;
+  final String? hintTitle;
+  final Widget? leftWidget;
+  final Widget? rightWidget;
+  final Widget? middleWidget;
+  final Widget? extendWidget;
+  final CrossAxisAlignment? crossAxisAlignmentMainRow;
+  final TextStyle? titleTextStyle;
+  final Color? backgroundColor;
+  final double? radius;
+  final double? paddingBottom;
+  final List<BoxShadow>? shadowHeader;
+  final Function()? onPressBack;
+  final bool showBackButton;
+
+  const AppHeader({
+    Key? key,
+    this.title,
+    this.leftWidget,
+    this.rightWidget,
+    this.middleWidget,
+    this.extendWidget,
+    this.crossAxisAlignmentMainRow,
+    this.hintContent,
+    this.hintTitle,
+    this.onPressBack,
+    this.titleTextStyle,
+    this.backgroundColor,
+    this.radius,
+    this.paddingBottom,
+    this.shadowHeader,
+    this.showBackButton = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(
+        0.0,
+        MediaQuery.of(context).padding.top + 6.0.sp,
+        0.0,
+        paddingBottom ?? 12.0.sp,
+      ),
+      decoration: BoxDecoration(
+        color: backgroundColor ?? AppColor.white,
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(radius ?? 12.0.sp),
+          bottomLeft: Radius.circular(radius ?? 12.0.sp),
+        ),
+        boxShadow: shadowHeader,
+      ),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: crossAxisAlignmentMainRow ?? CrossAxisAlignment.center,
+            children: [
+              showBackButton
+                  ? SizedBox(
+                      width: 40.0.sp,
+                      height: 40.0.sp,
+                    )
+                  : leftWidget ??
+                      AppTouchable(
+                        width: 40.0.sp,
+                        height: 40.0.sp,
+                        padding: EdgeInsets.all(5.0.sp),
+                        margin: EdgeInsets.only(left: 10.sp),
+                        onPressed: onPressBack ?? Get.back,
+                        child: AppImageWidget.asset(
+                          path: AppImage.icBack,
+                        ),
+                      ),
+              Expanded(
+                child: middleWidget ??
+                    Text(
+                      title ?? '',
+                      textAlign: TextAlign.center,
+                      style: titleTextStyle ?? AppTextTheme.headerTextStyle,
+                    ),
+              ),
+              rightWidget ??
+                  SizedBox(
+                    width: 40.0.sp,
+                    height: 40.0.sp,
+                  ),
+            ],
+          ),
+          extendWidget ?? const SizedBox.shrink(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/app_image.dart
@@ -0,0 +1,12 @@
+class AppImage {
+  AppImage._();
+
+  static const String _svgPath = "assets/svg/";
+  static const String _pngPath = "assets/image/";
+
+  static String icBack = "${_svgPath}ic_back.svg";
+
+  static String imgHeartRate = "${_pngPath}img_heart_rate.png";
+  static String imgBloodPressure = "${_pngPath}img_blood_pressure.png";
+  static String imgWeightBmi = "${_pngPath}img_weight_bmi.png";
+}
