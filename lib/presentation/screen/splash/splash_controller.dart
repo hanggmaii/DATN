31615ae3b0c5311b62c0ca9a4d3b2fcead24df2f
hanggmaii <hangmtn.00@gmail.com>
2024-05-10 18:58:53 +0700

    :update: update splash controller

--- lib/presentation/screen/splash/splash_controller.dart
+++ lib/presentation/screen/splash/splash_controller.dart
@@ -1,3 +1,18 @@
-import 'package:datn/presentation/base/base_controller.dart';
+import 'package:get/get.dart';
 
-class SplashController extends BaseController {}
+import '../../base/base_controller.dart';
+import '../../route/app_route.dart';
+
+class SplashController extends BaseController {
+  @override
+  void onReady() {
+    super.onReady();
+
+    Future.delayed(
+      const Duration(milliseconds: 1000),
+      () {
+        Get.offAndToNamed(AppRoute.homeScreen);
+      },
+    );
+  }
+}
