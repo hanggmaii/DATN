872e235a65d44a3e07d43c2ddb217ba937289797
hanggmaii <hangmtn.00@gmail.com>
2024-05-14 04:03:02 +0700

    :update: update app image

--- lib/utils/app_image.dart
+++ lib/utils/app_image.dart
@@ -4,9 +4,12 @@ class AppImage {
   static const String _svgPath = "assets/svg/";
   static const String _pngPath = "assets/image/";
 
-  static String icBack = "${_svgPath}ic_back.svg";
+  static const String icBack = "${_svgPath}ic_back.svg";
 
-  static String imgHeartRate = "${_pngPath}img_heart_rate.png";
-  static String imgBloodPressure = "${_pngPath}img_blood_pressure.png";
-  static String imgWeightBmi = "${_pngPath}img_weight_bmi.png";
+  static const String imgHeartRate = "${_pngPath}img_heart_rate.png";
+  static const String imgBloodPressure = "${_pngPath}img_blood_pressure.png";
+  static const String imgWeightBmi = "${_pngPath}img_weight_bmi.png";
+  static const String imgAdd = "${_pngPath}img_add.png";
+  static const String imgHeartRateTutorial = "${_pngPath}img_heart_rate_tutorial.png";
+  static const String imgSplash = "${_pngPath}img_splash.png";
 }
