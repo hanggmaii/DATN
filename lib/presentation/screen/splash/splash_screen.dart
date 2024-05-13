66fb38915d264393860a3b1f63a098fc506f2d7e
hanggmaii <hangmtn.00@gmail.com>
2024-05-14 04:05:29 +0700

    :white_check_mark: splash screen

--- lib/presentation/screen/splash/splash_screen.dart
+++ lib/presentation/screen/splash/splash_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
+import '../../../utils/app_image.dart';
 import '../../base/base_screen.dart';
+import '../../theme/app_color.dart';
+import '../../theme/app_text_theme.dart';
 import '../../widget/app_container.dart';
+import '../../widget/app_image_widget.dart';
 import 'splash_controller.dart';
 
 class SplashScreen extends BaseScreen<SplashController> {
@@ -14,11 +19,19 @@ class SplashScreen extends BaseScreen<SplashController> {
       child: SizedBox(
         width: Get.width,
         height: Get.height,
-        child: const Column(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Splash screen"),
+            AppImageWidget.asset(
+              path: AppImage.imgSplash,
+              height: 130.0.sp,
+              width: 130.0.sp,
+            ),
+            Text(
+              "HealthPulse",
+              style: AppTextTheme.fw600ts20(AppColor.primaryColor),
+            )
           ],
         ),
       ),
